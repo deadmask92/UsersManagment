3e6a6ce43ffbfd19aa8780528f73a53096022e1c
mac-max <luca.anghileri@ff3d.com>
2014-11-25 18:06:39 +0100

    a

---
+++ UsersManagement/UsersManagement/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UsersManagement.Models;
+
+namespace UsersManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginForm loginForm)
+        {
+            if ((loginForm.Username == "LUCA") && (loginForm.Password == "LUCA"))
+            {
+
+            }
+            return View();
+        }
+
+	}
+}
---
+++ UsersManagement/UsersManagement/Models/LoginForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UsersManagement.Models
+{
+    public class LoginForm
+    {
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
